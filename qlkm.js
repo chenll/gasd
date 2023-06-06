a3b595b0b57a823f10a088da9406e039247567e7
chenll <woshicll88@126.com>
2023-06-06 10:54:24 +0800

    Update qlkm.js

--- qlkm.js
+++ qlkm.js
@@ -8,6 +8,10 @@ hostname = qadx*.qinlinad.com, mall-dsp*.qinlinkeji.com
 
 *************************************/
 
-var body = $response.body
-delete body.result.ads;
-$done({ body: JSON.stringify(obj) });
+let obj=JSON.parse($response.body);
+/*京喜APP*/
+if (/^https?:\/\/api\.m\.jd\.com\/api\?functionId=delivery_show/.test($request.url)) { 
+    obj.data.materialList.startTime=3667476800000;
+    obj.data.materialList.endTime  =3667908800000;
+}
+$done({body:JSON.stringify(obj)});
