42ed13899454754733bff48806291bf890d1ecf4
chenll <woshicll88@126.com>
2024-05-21 12:26:35 +0800

    Update qlkm.js

--- qlkm.js
+++ qlkm.js
@@ -1,13 +1,9 @@
-
-
-[rewrite_local]
-^https?:\/\/(qadx.*\.qinlinad|mall-dsp.*\.qinlinkeji)\.com url script-response-body https://raw.githubusercontent.com/chxm1023/Advertising/main/xcx/qlkm.js
-
-[mitm]
-hostname = qadx*.qinlinad.com, mall-dsp*.qinlinkeji.com
-
-*************************************/
-
-let obj=JSON.parse($response.body);
-ob.result.ads = [];
-$done({body:JSON.stringify(obj)});
+var body = $response.body;
+body = body.replace(/\"list"./g, delete '\"list"');
+body = body.replace(/\"newYearSkinBannerUrl":".*?"/g, '\"newYearSkinBannerUrl":""');
+body = body.replace(/\"defaultBannerUrl":".*?"/g, '\"defaultBannerUrl":""');
+body = body.replace(/\"groupId":"\d+"/g, '\"groupId":"0"');
+body = body.replace(/\"mainSwitch":\d+/g, '\"mainSwitch":0');
+body = body.replace(/\"slotId":".*?"/g, '\"slotId":""');
+body = body.replace(/\"slotModeSort":\d+/g, '\"slotModeSort":0');
+$done({body});
