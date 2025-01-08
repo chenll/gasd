2aa596ebfce496ebb8f0a1cb2845cf072303df73
chenll <woshicll88@126.com>
2025-01-08 16:37:22 +0800

    Delete qlkm.js

--- qlkm.js
+++
@@ -1,9 +0,0 @@
-var body = $response.body;
-body = body.replace(/\"list"./g, delete '\"list"');
-body = body.replace(/\"newYearSkinBannerUrl":".*?"/g, '\"newYearSkinBannerUrl":""');
-body = body.replace(/\"defaultBannerUrl":".*?"/g, '\"defaultBannerUrl":""');
-body = body.replace(/\"groupId":"\d+"/g, '\"groupId":"0"');
-body = body.replace(/\"mainSwitch":\d+/g, '\"mainSwitch":0');
-body = body.replace(/\"slotId":".*?"/g, '\"slotId":""');
-body = body.replace(/\"slotModeSort":\d+/g, '\"slotModeSort":0');
-$done({body});
